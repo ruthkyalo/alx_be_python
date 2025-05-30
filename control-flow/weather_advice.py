4d541797cacd645089235769783383a9fa4c068c
Ruth <ruthkyalo21@gmail.com>
2025-05-30 17:25:42 +0300

    add a weather recommendation program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,18 @@
+current_weather = input("What's the weather like today? (sunny/rainy/cold):")
+
+# Provide Clothing Recommendations:
+# Based on the user’s input, your program will recommend different types of clothing:
+# If the weather is “sunny”, recommend: Wear a t-shirt and sunglasses.
+# If the weather is “rainy”, recommend: Don't forget your umbrella and a raincoat.
+# If the weather is “cold”, recommend: Make sure to wear a warm coat and a scarf.
+# Include an else statement that handles unexpected input by printing: Sorry, I don't have recommendations for this weather.
+
+# Print the clothing recommendation based on the weather condition provided by the user.
+if current_weather == "sunny":
+    print("Wear a t-shirt and sunglasses")
+elif current_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat") 
+elif current_weather == "cold":
+    print("Make sure to wear a warm coat and scarf")  
+else:
+    print("Sorry, I don't have recommendations for this weather")     

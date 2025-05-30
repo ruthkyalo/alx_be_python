17e449bfd2cacaa43b8977f6e3d398fac512f811
Ruth <ruthkyalo21@gmail.com>
2025-05-30 18:32:45 +0300

    add Drawing Patterns with Nested Loops

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,27 @@
+task = input("Enter task description: ")
+task_priority = input("what is the task priority (high, medium, low): ")
+time_bound = input("is the task time bound (yes or no)")
+# Use a Match Case statement to react differently based on the task’s priority.
+# Within the Match Case or after, use an if statement to modify the reminder if the task is time-bound.
+# Print a reminder about the task that includes its priority level and whether immediate action is required based on time sensitivity.
+# A message should be ‘that requires immediate attention today!’
+
+match task_priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a high priority task Consider completing it when you have free time.") 
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a medium priority task but does not require immidiate action!") 
+    case "low":
+        if time_bound == "yes":
+            print(f"Note: '{task}' is a low priority task that requires immediate attention today!")
+        else:
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.") 
+
+    case _:
+        print("invalid input")                         

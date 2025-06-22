bddb903137dec82b15226d332679746b9759ea50
Ruth <ruthkyalo21@gmail.com>
2025-06-22 08:23:18 +0300

    Add Book class with __init__, __del__, __str__, and __repr__ methods

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,11 +1,11 @@
-# library_system.py
+
 
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -14,7 +14,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -23,17 +23,17 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
     def __init__(self):
-        self.books = []  # Composition: List of Book (or subclass) instances
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)  # Uses __str__ automatically
